c13cb223e419e684209571aaf36db4ed3b564b6d
yu.matsushita <yu.matsushita@jp.oro.com>
2015-07-01 14:48:03 +0900

    Port implementation.

--- src/Enclosure.cs
+++ src/Enclosure.cs
@@ -1,6 +1,45 @@
-﻿namespace EnclosedWithThanks.Enclosure
+﻿using System;
+
+namespace EnclosedWithThanks.Enclosure
 {
-    public class Enclosure
+    public class Enclosing : IDisposable
     {
+        public static T GetEnclosure<T>(Action<T> onOpen, Action<T> onClose)
+            where T : Enclosing, new()
+        {
+            T enclosing = new T();
+            enclosing.Opening = () => onOpen(enclosing);
+            enclosing.Closing = () => onClose(enclosing);
+            enclosing.DoOpen();
+            return enclosing;
+        }
+
+        public static Enclosing GetEnclosure(Action<Enclosing> onOpen, Action<Enclosing> onClose)
+        {
+            return GetEnclosure<Enclosing>(onOpen, onClose);
+        }
+
+        public void Dispose()
+        {
+            DoClose();
+        }
+
+        protected void DoOpen()
+        {
+            if (Opening != null)
+            {
+                Opening();
+            }
+        }
+        protected void DoClose()
+        {
+            if (Closing != null)
+            {
+                Closing();
+            }
+        }
+
+        protected Action Opening { get; set; }
+        protected Action Closing { get; set; }
     }
 }
--- test/Enclosure.cs
+++ test/Enclosure.cs
@@ -1,6 +1,101 @@
-﻿namespace EnclosedWithThanks.Spec
+﻿using NSpec;
+using EnclosedWithThanks.Enclosure;
+
+namespace EnclosedWithThanks.Spec
 {
-    public class Enclosure
+    class describe_Enclosing : nspec
     {
+
+        class CustomEnclosing : Enclosing
+        {
+            private int myNumber = 0;
+            public int MyNumber
+            {
+                get { return myNumber; }
+                set { myNumber = value; }
+            }
+        }
+
+
+        void when_opening_action_is_supplied()
+        {
+            it["should trigger onOpen action when opening"] = () => {
+                int i = 0;
+                using (
+                    Enclosing.GetEnclosure(
+                        onOpen: (context) => { i++; },
+                        onClose: (context) => {}))
+                {
+                    i.should_be(1);
+                }
+            };
+
+            it["should trigger onOpen action when opening within custom subclass"] = () => {
+                using (
+                    var enclosing = Enclosing.GetEnclosure<CustomEnclosing>(
+                        onOpen: (context) => { context.MyNumber++; },
+                        onClose: (context) => {}))
+                {
+                    enclosing.MyNumber.should_be(1);
+                }
+            };
+        }
+
+
+        void when_closing_action_is_supplied()
+        {
+            it["should trigger onClose action when closing"] = () => {
+                int i = 0;
+                using (
+                    Enclosing.GetEnclosure(
+                        onOpen: (context) => {},
+                        onClose: (context) => { i++; }))
+                {
+                    i.should_be(0);
+                }
+                i.should_be(1);
+            };
+
+            it["should trigger onClose action when closing within custom subclass"] = () => {
+                int myNumberCopied = 0;
+
+                using (
+                    var enclosing = Enclosing.GetEnclosure<CustomEnclosing>(
+                        onOpen: (context) => {},
+                        onClose: (context) => { context.MyNumber++; myNumberCopied = context.MyNumber; }))
+                {
+                    enclosing.MyNumber.should_be(0);
+                }
+                myNumberCopied.should_be(1);
+            };
+        }
+
+
+        void when_opening_and_closing_actions_are_supplied()
+        {
+            it["should trigger both"] = () => {
+                int i = 0;
+                using (
+                    Enclosing.GetEnclosure(
+                        onOpen: (context) => { i++; },
+                        onClose: (context) => { i++; }))
+                {
+                    i.should_be(1);
+                }
+                i.should_be(2);
+            };
+
+            it["should trigger both within custom subclass"] = () => {
+                int myNumberCopied = 0;
+                using (
+                    var enclosing = Enclosing.GetEnclosure<CustomEnclosing>(
+                        onOpen: (context) => { context.MyNumber++; },
+                        onClose: (context) => { context.MyNumber++; myNumberCopied = context.MyNumber; }))
+                {
+                    enclosing.MyNumber.should_be(1);
+                }
+                myNumberCopied.should_be(2);
+            };
+        }
     }
 }
